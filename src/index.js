682791d63822676796032544daa9585e34a1d0c8
B1906578 <thoab1906578@student.ctu.edu.vn>
2023-09-30 15:13:37 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const dotenv = require('dotenv');
 const  mongoose = require("mongoose");
+const routes = require('./routes')
+const bodyParser = require('body-parser')
+
 dotenv.config()
 
 const app = express()
-const port =process.env.PORT || 3002
+const port = process.env.PORT || 3002
+app.use(bodyParser.json())
 
-app.get('/',(req,res) => {
-   res.send('Hello world 111')
-})
+routes(app);
 
-console.log('process.env.MONGO_DB',process.env.MONGO_DB)
+mongoose.connect(`${process.env.MONGO_DB}`)
+  .then(() => {
+    console.log('Connect DB success!')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
-mongoose.connect(`mongodb+srv://lekimthoa1223:${process.env.MONGO_DB}@cluster0.rilr6c5.mongodb.net/?retryWrites=true&w=majority`)
-.then(() => {
-  console.log('Connect DB success!')
-})
-.catch((err) => {
-  console.log(err)
-})
-app.listen(port,() => {
-  console.log('Server is running in port: ',+port)
+app.listen(port, () => {
+  console.log('Server is running in port: ', + port)
 })
---
+++ src/services/UserService.js
@@ -0,0 +1,42 @@
+const User = require("../models/UsersModel")
+
+const createUser = (newUser) => {
+  return new Promise ( async (resolve, reject) => {
+    const { name, email, password, confirmPassword, phone } = newUser
+    try{
+      const checkUser = await User.findOne({
+        email: email
+      })
+      if(checkUser !== null){
+        resolve({
+          status: 'OK',
+          message: 'The email is already'
+        })
+      }
+      // resolve({
+      //   data: checkUser
+      // })
+      const createUser = await User.create({
+        name, 
+        email, 
+        password, 
+        confirmPassword, 
+        phone
+      })
+      if(createUser) {
+        resolve({
+          status: 'OK',
+          message: 'SUCCESS',
+          data: createUser
+          
+        })
+      }
+    }catch (e) {
+      reject(e)
+    }
+  })
+}
+
+module.exports = {
+  createUser
+}
