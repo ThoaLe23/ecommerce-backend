306cdf3e01b6c36afea8a5a3992324c44a6ea9d8
ThoaLe23 <lekimthoa.1223@gmail.com>
2023-11-08 21:13:11 +0700

    Update code

--- src/services/JwtService.js
+++ src/services/JwtService.js
@@ -8,7 +8,7 @@ const genneralAcessToken = async (payload) => {
       ...payload
     },
     process.env.ACCESS_TOKEN,
-    { expiresIn: '30s' }
+    { expiresIn: '30m' }
   );
   return access_token;
 };
